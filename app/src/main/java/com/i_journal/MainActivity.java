66ad7bbf05ed40a22219cd96b92c8f7f49fea27e
NgocPhu2009 <NgocPhu2009@users.noreply.github.com>
2019-12-07 10:06:12 +0700

    added onItemClickListener for the listview

--- app/src/main/java/com/i_journal/MainActivity.java
+++ app/src/main/java/com/i_journal/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -83,173 +82,175 @@ public class MainActivity extends AppCompatActivity implements FirebaseHelperLis
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         mAuth = FirebaseAuth.getInstance();
 
-//        setupItemEvent();}
+//        setupItemEvent();
 
 
-        @Override
-        protected void onStart () {
-            super.onStart();
-            lv_post = entryFragment.getLv_post();
-            setupFirebase();
-            registerForContextMenu(lv_post);
-        }
-        @Override
-        public void onCreateContextMenu ( final ContextMenu menu, final View v,
-        final ContextMenu.ContextMenuInfo menuInfo){
-            super.onCreateContextMenu(menu, v, menuInfo);
-            menu.add(0, 0, 0, "Update Post");
-            menu.add(0, 1, 1, "Delete Post");
+    }
 
-        }
-        @Override
-        public boolean onContextItemSelected (MenuItem menuItem){
-            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuItem.getMenuInfo();
-            final Post selectedPost = (Post) this.lv_post.getItemAtPosition(info.position);
-            switch (menuItem.getItemId()) {
-                case 0:
-                    Intent intent = new Intent(getBaseContext(), UpdatePostActivity.class);
-                    intent.putExtra("UPDATEPOST", selectedPost);
-                    startActivityForResult(intent, UPDATE_POST_REQUEST);
-                    break;
-                case 1: {
-                    new AlertDialog.Builder(this)
-                            .setMessage("Do you want to delete this contact")
-                            .setCancelable(false)
-                            .setNegativeButton("No", null)
-                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    firebaseHelper.deletePost(currentFirebaseUser.getUid(), selectedPost.getKey());
-                                }
-                            }).show();
-                    break;
-                }
-
-                default:
-                    break;
-            }
-            return true;
-        }
-
-        @Override
-        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent intent){
-            super.onActivityResult(requestCode, resultCode, intent);
-            if (requestCode == ADD_POST_REQUEST) {
-                if (resultCode == RESULT_OK) {
-                    Bundle bundle = intent.getExtras();
-                    Post newPost = (Post) bundle.getSerializable("NEWPOST");
-                    firebaseHelper.writePost(currentFirebaseUser.getUid(), newPost);
-                }
-            }
-            if (requestCode == UPDATE_POST_REQUEST) {
-                if (resultCode == RESULT_OK) {
-                    Bundle bundle = intent.getExtras();
-                    Post newPost = (Post) bundle.getSerializable("NEWPOST");
-                    firebaseHelper.updatePost(currentFirebaseUser.getUid(), newPost);
-                }
-            }
-        }
-
-        private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-                = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        lv_post = entryFragment.getLv_post();
+        setupFirebase();
+        registerForContextMenu(lv_post);
+        alPost = firebaseHelper.readPost(currentFirebaseUser.getUid());
+        lv_post.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                Fragment fragment;
-                switch (menuItem.getItemId()) {
-                    case R.id.entry:
-                        fragment = new EntryFragment();
-                        loadFragment(fragment);
-                        onPostsChange(alPost);
-                        return true;
-                    case R.id.stats:
-                        fragment = new StatsFragment(currentFirebaseUser, firebaseHelper);
-                        loadFragment(fragment);
-                        return true;
-                    case R.id.calendar:
-                        fragment = new CalendarFragment();
-                        loadFragment(fragment);
-                        return true;
-                    case R.id.logout:
-                        signOut();
-                        return true;
-                }
-                return false;
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final Post post = (Post)alPost.get(i);
+                Log.d("TITLE", "onItemClick: " + post.getTitle());
+                Intent intent = new Intent(MainActivity.this, UpdatePostActivity.class);
+                intent.putExtra("UPDATEPOST",post);
+                startActivityForResult(intent, UPDATE_POST_REQUEST);
             }
-        };
+        });
+    }
+    @Override
+    public void onCreateContextMenu(final ContextMenu menu, final View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0,0,0,"Update Post");
+        menu.add(0,1,1,"Delete Post");
 
-        private void setupFirebase () {
-            FirebaseApp.initializeApp(this);
-            //mDatabase = FirebaseDatabase.getInstance().getReference();
-            firebaseHelper = new FirebaseHelper(FirebaseDatabase.getInstance().getReference(), this);
-            currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-            if (currentFirebaseUser != null) {
-                String email = currentFirebaseUser.getEmail();
-                System.out.println("EMAIL:  " + email);
-                firebaseHelper.readPost(currentFirebaseUser.getUid());
-            } else {
-                System.out.println("Do not get current Firebase user");
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem menuItem){
+        AdapterView.AdapterContextMenuInfo info=(AdapterView.AdapterContextMenuInfo)menuItem.getMenuInfo();
+        final Post selectedPost=(Post) this.lv_post.getItemAtPosition(info.position);
+        switch (menuItem.getItemId()) {
+            case 0:
+                Intent intent = new Intent(getBaseContext(), UpdatePostActivity.class);
+                intent.putExtra("UPDATEPOST", selectedPost);
+                startActivityForResult(intent, UPDATE_POST_REQUEST);
+                break;
+            case 1:{
+                new AlertDialog.Builder(this)
+                        .setMessage("Do you want to delete this contact")
+                        .setCancelable(false)
+                        .setNegativeButton("No",null)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                firebaseHelper.deletePost(currentFirebaseUser.getUid(),selectedPost.getKey());
+                            }
+                        }).show();
+                break;
+            }
+
+            default:
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == ADD_POST_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Bundle bundle = intent.getExtras();
+                Post newPost = (Post) bundle.getSerializable("NEWPOST");
+                firebaseHelper.writePost(currentFirebaseUser.getUid(), newPost);
             }
         }
+        if (requestCode == UPDATE_POST_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Bundle bundle = intent.getExtras();
+                Post newPost = (Post) bundle.getSerializable("NEWPOST");
+                firebaseHelper.updatePost(currentFirebaseUser.getUid(), newPost);
+            }
+        }
+    }
 
-        public void setupItemEvent () {
-            lv_post.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    final Post post = (Post) alPost.get(i);
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment;
+            switch (menuItem.getItemId()) {
+                case R.id.entry:
+                    fragment = new EntryFragment();
+                    loadFragment(fragment);
+                    firebaseHelper.readPost(currentFirebaseUser.getUid());
+                    return true;
+                case R.id.stats:
+                    fragment = new StatsFragment(currentFirebaseUser,firebaseHelper);
+                    loadFragment(fragment);
+                    return true;
+                case R.id.calendar:
+                    fragment = new CalendarFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.logout:
+                    signOut();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void setupFirebase() {
+        FirebaseApp.initializeApp(this);
+        //mDatabase = FirebaseDatabase.getInstance().getReference();
+        firebaseHelper = new FirebaseHelper(FirebaseDatabase.getInstance().getReference(), this);
+        currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentFirebaseUser != null) {
+            String email = currentFirebaseUser.getEmail();
+            System.out.println("EMAIL:  " + email);
+            firebaseHelper.readPost(currentFirebaseUser.getUid());
+        } else {
+            System.out.println("Do not get current Firebase user");
+        }
+    }
+
+    public void setupItemEvent(){
+        lv_post.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final Post post = (Post)alPost.get(i);
 //                Intent intent = new Intent(WallActivity.this, HomeActivity.class);
 //                intent.putExtra("KEY",post.getKey());
 //                startActivity(intent);
-                }
-            });
-        }
+            }
+        });
+    }
 
-        private void loadFragment (Fragment fragment){
-            // load fragment
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.frame_container, fragment);
-            transaction.addToBackStack(null);
-            transaction.commit();
-        }
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
-        private void signOut () {
-            // Firebase sign out
-            mAuth.signOut();
-            // Google sign out
-            mGoogleSignInClient.signOut().addOnCompleteListener(this,
-                    new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            Intent intent = new Intent(getBaseContext(), LoginActivity.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                    });
-        }
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+    }
 
-        @Override
-        public void onPostsChange (List < Post > alPost) {
-            adapter = new PostAdapter(getBaseContext(), R.layout.post_list_item, alPost);
-            lv_post.setAdapter(adapter);
-            adapter.notifyDataSetChanged();
-            System.out.println("SIZEEEEE 1 " + alPost.size());
-        }
-
-        @Override
-        public void onClick (View view){
-
-        }
+    @Override
+    public void onPostsChange(List<Post> alPost) {
+        adapter = new PostAdapter(getBaseContext(), R.layout.post_list_item, alPost);
+        lv_post.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        System.out.println("SIZEEEEE 1 " + alPost.size());
     }
 
     @Override
     public void onClick(View view) {
 
     }
-
-    @Override
-    public void onPostsChange(List<Post> alPost) {
-
-    }
 }
+
 interface FirebaseHelperListener {
     void onPostsChange(List<Post> alPost);
 }
